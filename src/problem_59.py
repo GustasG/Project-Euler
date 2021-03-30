91a1f7f9db54036dc692b2afe4947e939b0b0e82
Gustas Gedminas <gustas.gedminas@gmail.com>
2021-03-30 16:08:23 +0300

    Simplified reading code

--- src/problem_59.py
+++ src/problem_59.py
@@ -1,35 +1,40 @@
 #!/usr/bin/python3
 
-from typing import List, Iterator
+from typing import Any, List, Iterator
 from itertools import permutations
 import string
 import os
 
+class CircularBuffer:
+    def __init__(self, values: List[Any]) -> None:
+        self.values = values
 
-def get_values(file_name: str) -> List[int]:
+    def __getitem__(self, key: int) -> Any:
+        return self.values[key % len(self.values)]
+
+
+def read_file(file_name: str) -> List[int]:
     with open(file_name, 'r') as f:
         numbers = f.read().split(',')
         return list(map(int, numbers))
 
-
 def decrypt(text: List[int], password: str) -> str:
-    numeric_password = list(map(ord, password))
+    numeric_password = CircularBuffer(list(map(ord, password)))
 
-    return ''.join(chr(text[i] ^ numeric_password[i % len(numeric_password)]) for i in range(len(text)))
+    return ''.join(chr(text[i] ^ numeric_password[i]) for i in range(len(text)))
 
 
 def bruteforce(values: List[int]) -> Iterator[str]:
     for password in permutations('abcdefghijklmnopqrstuvwxyz', 3):
-        decrypted = decrypt(values, password)
+        yield decrypt(values, password)
 
-        if decrypted.find('the') >= 0 and all(c in string.printable for c in decrypted):
-            yield decrypted
 
 def main():
-    values = get_values(os.path.join('res', 'problem_59_cipher.txt'))
+    values = read_file(os.path.join('res', 'problem_59_cipher.txt'))
 
-#    for test in bruteforce(values):
-#        print(test)
+    # for test in bruteforce(values):
+    #     if test.find('the') >= 0 and all(c in string.printable for c in test):
+    #         print(test)
 
     text = decrypt(values, 'exp')
 
