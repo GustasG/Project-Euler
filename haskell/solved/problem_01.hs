88adbcc75b85b6a35f5a087c6a5f0bcd903378a6
Gustas Gedminas <gustasg@aai-labs.com>
2021-09-06 00:10:00 +0300

    solved 1st problem with haskell

---
+++ haskell/solved/problem_01.hs
@@ -0,0 +1,16 @@
+summation :: Integer -> Integer -> Integer
+summation k n =
+    k * p * (p + 1) `div` 2
+    where
+    p = (n - 1) `div` k
+
+
+multiples :: Integer -> Integer
+multiples number =
+    summation 3 number +
+    summation 5 number -
+    summation 15 number
+
+
+main = do
+    print(multiples 1000)
