d76bf396a127bf9b234b26e3f89a0582ca7b1345
Gustas Gedminas <gustasg@aai-labs.com>
2021-09-12 23:33:13 +0300

    started 250th problem

---
+++ project-euler-python/unsolved/problem_250.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+def calculate_residues(m: int, n: int) -> List[int]:
+    residues = [0] * n
+
+    for i in range(1, m + 1):
+        residues[pow(i, i, n)] += 1
+
+    return residues
+
+
+def main() -> None:
+    m = 250_250
+    n = 250
+
+    residues = calculate_residues(m, n)
+
+    print(residues)
+
+
+if __name__ == '__main__':
+    main()
